88b49457d78b296d7d193abb4fa2cf8d53dff614
Miwand Najafe <miwand.n@ecobee.com>
2019-11-20 17:55:51 -0500

    adding ios_client support for SPM

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:ecobee/ios-eventsource.git", from: "4.0.2")
+        .package(url: "git@github.com:ecobee/ios-eventsource.git", .branch("mn/SPMTest"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
